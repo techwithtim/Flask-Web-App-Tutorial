4d92e3462aaaeebe9a165b7cd4470629bb294cd1
Valentina_pro <33911680+Drc-DEV@users.noreply.github.com>
2021-02-02 18:11:17 +0100

    Fix #2
    https://github.com/techwithtim/Flask-Web-App-Tutorial/issues/2

--- website/auth.py
+++ website/auth.py
@@ -59,7 +59,7 @@ def sign_up():
                 password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
 
