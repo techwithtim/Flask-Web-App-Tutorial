74f88993a294baececba6fe069b414fd4f0c0ea4
Dimi <ilev997@gmail.com>
2022-11-25 13:33:31 +0100

    db

--- website/__init__.py
+++ website/__init__.py
@@ -21,7 +21,9 @@ def create_app():
 
     from .models import User, Note
 
-    create_database(app)
+    #create_database(app)
+    with app.app_context():
+      db.create_all()
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
