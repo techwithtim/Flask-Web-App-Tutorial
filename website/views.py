7ca5696a22e7beb09282191ccecd033c31fc192a
ibroyles <ibroyles2@huskers.unl.edu>
2023-04-30 16:10:14 -0500

    Set up front end functionality

--- website/views.py
+++ website/views.py
@@ -1,6 +1,4 @@
 from flask import Blueprint, render_template, request, flash, jsonify
-from flask_login import login_required, current_user
-from .models import Note
 from . import db
 import json
 
@@ -8,30 +6,29 @@ views = Blueprint('views', __name__)
 
 
 @views.route('/', methods=['GET', 'POST'])
-@login_required
 def home():
-    if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
+    #if request.method == 'POST': 
+    #     note = request.form.get('note')#Gets the note from the HTML 
 
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
-        else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
-            db.session.commit()
-            flash('Note added!', category='success')
+    #     if len(note) < 1:
+    #         flash('Note is too short!', category='error') 
+    #     else:
+    #         new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
+    #         db.session.add(new_note) #adding the note to the database 
+    #         db.session.commit()
+    #         flash('Note added!', category='success')
 
-    return render_template("home.html", user=current_user)
+    return render_template("answer.html")
 
 
-@views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
+# @views.route('/delete-note', methods=['POST'])
+# def delete_note():  
+#     note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+#     noteId = note['noteId']
+#     note = Note.query.get(noteId)
+#     if note:
+#         if note.user_id == current_user.id:
+#             db.session.delete(note)
+#             db.session.commit()
 
-    return jsonify({})
+#     return jsonify({})
