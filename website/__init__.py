9c70abc76a6d3cb9361dd989e5c5375786c843ad
DrashtiS <94853646+DrashtiSanjayShah@users.noreply.github.com>
2023-08-04 13:02:58 +0530

    Update __init__.py
    Made changes in def create_database()

--- website/__init__.py
+++ website/__init__.py
@@ -36,6 +36,7 @@ def create_app():
 
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+    if not os.path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+            print("Created Database!")
