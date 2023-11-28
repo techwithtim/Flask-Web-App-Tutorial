e51c05ddc2f5c83ac94c46c1f7c99fa4fd6d8e9f
ch1llc0d3 <128004915+ch1llc0d3@users.noreply.github.com>
2023-11-28 08:04:52 -0300

    Update auth.py
    change of code to make it work!

--- website/auth.py
+++ website/auth.py
@@ -55,8 +55,10 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
+            #new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+            #    password1, method='sha256'))
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1))
+
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
