b282ec81b4fcc781c3e29d53b2eab91cac0c2bed
ussssf <usofa.1223@gmail.com>
2024-02-18 19:38:31 +0100

    fixed : invalid hashmethod sha256

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db   ##means from __init__.py import db
+from . import db  # means from __init__.py import db
 from flask_login import login_user, login_required, logout_user, current_user
 
 
@@ -56,7 +56,7 @@ def sign_up():
             flash('Password must be at least 7 characters.', category='error')
         else:
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
+                password1))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
