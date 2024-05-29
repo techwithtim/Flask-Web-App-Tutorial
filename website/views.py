7240748f07abc8401412456090d5f08b75de3c06
Danny Kardhashi <dannykardhashi@Dannys-MacBook-Pro-4.local>
2024-05-29 15:06:12 -0400

    q form

--- website/views.py
+++ website/views.py
@@ -1,28 +1,30 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, redirect, jsonify
 from flask_login import login_required, current_user
-from .models import Note
+from .models import QuestionnaireResponse
 from . import db
 import json
+from .forms import QuestionnaireForm
 
 views = Blueprint('views', __name__)
-
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
+    form = QuestionnaireForm()
+    if form.validate_on_submit():
+        response = QuestionnaireResponse(
+            user_id=current_user.id,
+            question1=form.question1.data,
+            question2=form.question2.data,
+            question3=form.question3.data
+            # Add more fields as needed
+        )
+        db.session.add(response)
+        db.session.commit()
+        flash('Thank you for completing the questionnaire!', 'success')
+        return redirect(url_for('views.home'))
 
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
-        else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
-            db.session.commit()
-            flash('Note added!', category='success')
-
-    return render_template("home.html", user=current_user)
-
+    return render_template("home.html", user=current_user, form=form)
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():  
