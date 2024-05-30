bb9b97a0a9deb63748b79a302300c9f85e05e5b3
Danny Kardhashi <dannykardhashi@dannys-mbp-4.mynetworksettings.com>
2024-05-30 13:06:06 -0400

    w

--- website/forms.py
+++ website/forms.py
@@ -3,7 +3,11 @@ from wtforms import RadioField, SubmitField
 from wtforms.validators import DataRequired
 
 class QuestionnaireForm(FlaskForm):
-    question1 = RadioField('Question 1', choices=[('option1', 'Option 1'), ('option2', 'Option 2'), ('option3', 'Option 3')], validators=[DataRequired()])
-    question2 = RadioField('Question 2', choices=[('option1', 'Option 1'), ('option2', 'Option 2'), ('option3', 'Option 3')], validators=[DataRequired()])
-    question3 = RadioField('Question 3', choices=[('option1', 'Option 1'), ('option2', 'Option 2'), ('option3', 'Option 3')], validators=[DataRequired()])
+    question1 = RadioField('How many days a week would you like to workout?', choices=[('option1', '1'), ('option2', '2'), ('option3', '3'), 
+                                                                                       ('option4', '4'), ('option5', '5'), 
+                                                                                       ('option6', '6'), ('option7', '7')], validators=[DataRequired()])
+    question2 = RadioField('What body parts would you NOT like to exercise?', choices=[('option1', 'Chest'), ('option2', 'Back'), ('option3', 'Arms')
+                                                                                       , ('option4', 'Shoulders'), ('option5', 'Legs')], validators=[DataRequired()])
+    question3 = RadioField('Which body part would you like to focus on?', choices=[('option1', 'Chest'), ('option2', 'Back'), ('option3', 'Arms')
+                                                                                       , ('option4', 'Shoulders'), ('option5', 'Legs')], validators=[DataRequired()])
     submit = SubmitField('Submit')
